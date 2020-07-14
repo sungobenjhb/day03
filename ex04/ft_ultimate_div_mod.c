963a416d63b1d8caa3092994695eefd6f47d4c20
Suraya Ngobeni <sungoben@c4r11s3.wethinkcode.co.za>
2020-07-14 08:39:36 +0200

    ex04 done

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungoben <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 12:45:55 by sungoben          #+#    #+#             */
+/*   Updated: 2020/07/13 13:13:34 by sungoben         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */#include <unistd.h>
+void ft_ultimate_div_mod(int *a ,int *b)
+{
+	int save1;
+	int save2;
+	save1 = a / b;
+	save2 =a % b;
+	*a = save1;
+	*b = save2;
+}
+int main ()
+{
+	int x;
+	int y;
+	x = 7;
+	y = 3;
+	ft_ultimate_div_mod (&x,&y);
+	printf("%d,%d\n",x,y)
+	return 0;
+}
+
+
+
